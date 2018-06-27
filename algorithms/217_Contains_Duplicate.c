4b8318a88b7747787f1cdf26e84ac1e1b81a99fe
Jing <guozijing218@gmail.com>
2018-06-27 17:26:25 +0800

    feat(algor.c): add 217 Contains Duplicate solution

--- algorithms/217_Contains_Duplicate.c
+++ algorithms/217_Contains_Duplicate.c
@@ -1,17 +1,39 @@
 /*************************************************************************************** 
-Source : 
+Source : https://leetcode.com/problems/contains-duplicate/description/
 Author : Jing Kuo
 
 Description:
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 
 Example:
+    Input: [1,2,3,4]
+    Output: false
+
+    Input: [1,1,1,3,3,4,3,2,4,2]
+    Output: true
 
 Complexity:
-    Time:O()
-    Space:O()
+    Time:O(nlogn)
+    Space:O(1)
 
-Beats: % 
+Beats: 100% 
 
-Approach: 
+Approach: 1. This is sortubg+compare
+          2. Can use hash, T:O(n),S:O(1S)
 
 ***************************************************************************************/
+int cmp(const int *a, const int *b) {
+    return *a-*b;
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums,numsSize,sizeof(int),cmp);
+
+    for (int i = 1; i < numsSize; i++)
+    {
+        if(nums[i] == nums[i-1])    return true;
+    }
+
+    return false;
+}
